13fde4a09d2c276320cfb3527d12b6f2f667344e
kbaba1001 <kbaba1001@users.noreply.github.com>
2024-01-18 11:01:26 +0900

    chore: adjust filename and add license

--- src/index.js
+++ src/index.js
@@ -2,5 +2,3 @@ export default function main() {
 	console.log("hello world");
 	return "hello world";
 }
-
-main();
--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,6 @@
 import * as assert from "node:assert";
 import { test } from "node:test";
-import main from "../src/main.js";
+import main from "../src/index.js";
 
 test("should return hello world", () => {
 	assert.equal(main(), "hello world");
